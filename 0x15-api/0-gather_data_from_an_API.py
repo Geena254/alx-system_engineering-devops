2e5811ee57d2c4e117865fe31ba6dfd2efdd1382
Georgina Kimani <njokikimani001@gmail.com>
2024-06-03 15:40:21 -0700

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Accessing a REST API and returning information about todo lists of employees"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employeeID = sys.argv[1]
+    baseURL = "https://jsonplaceholder.typicode.com/users"
+    url = baseURL + "/" + employeeID
+
+    response = requests.get(url)
+    employeeName = response.json().get('name')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    tasks_done = []
+
+    for task in tasks:
+        if task.get('completed'):
+            tasks_done.append(task)
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
+
+    for task in tasks_done:
+        print("\t {}".format(task.get('title')))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Accessing a REST API and returning information about todo lists of employees,
+   then exporting the data in CSV format.
+"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employeeID = sys.argv[1]
+    baseURL = "https://jsonplaceholder.typicode.com/users"
+    url = baseURL + "/" + employeeID
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    with open('{}.csv'.format(employeeID), 'w') as file: # Create CSV file
+        for task in tasks: # Export task data to CSV
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employeeID, username, task.get('completed'),
+                               task.get('title')))
